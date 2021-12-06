f659d80b96b2295e40820c35b3a142247d42275a
Luu-Anh <74136601+luuanhvuong@users.noreply.github.com>
2021-12-06 09:46:16 +0100

    chgt

--- Panier.php
+++ Panier.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <head>
-<title>Paris Shopping</title>
+<title>Votre Panier</title>
 <meta charset="utf-8"/>
-<link href="prime.css" rel="stylesheet" type="text/css"/>
+<link href="panier.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
 <!-- Here is the wrapper area -->
@@ -14,31 +14,17 @@
  </div>
 
     <div class="topnav">
-        <a class="active" href="#home">Accueil</a>
+        <a  href="Accueil.html">Accueil</a>
         <a href="ToutParcourir.html">Tout Parcourir</a>
         <a href="#notif">Notifications</a>
-        <a href="Panier.html">Panier</a>
+        <a class="active" href="#Panier">Panier</a>
         <a href="#compte">Votre Compte</a>
       </div>
 
- <p>Bonjour ! Bienvenue sur le site de Paris Shopping ! Ici, vous pouvez trouver LA voiture qui vous correspond et à petits prix.
- Créez-vous un compte en tant qu'acheteur pour bénéficier de nos offres.
-<br>Si vous cherchez à vendre votre véhicule, créez un compte vendeur pour rejoindre notre belle équipe !<br> N'attendez plus !</p>
- <h2>Séléction du jour</h2><br>
  
- <p> La philosophie de Prime Properties est de promouvoir nos clients, pas nous-mêmes. </p>
- <p> Nous pouvons également vous aider à trouver la propriété qui répond à vos besoins: </p>
- <ul>
- <li>emplacement</li>
- <li>prix</li>
- <li>caractéristiques</li>
- </ul>
- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.371003025834!2d2.3221761153522955!3d48.851135379286745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671d39ebb8601%3A0xd99b7bf65c716272!2sLe%20Bon%20March%C3%A9!5e0!3m2!1sen!2sfr!4v1638711772998!5m2!1sen!2sfr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
- <p class="contact">Prime Properties<br>
- 67 avenue Marceau<br>
- 75015 Paris<br><br>
- (+33) 01 02 03 04 05 06
- </p>
+ <h2>VOTRE PANIER</h2><br>
+ 
+ 
  <!-- Here is the footer area -->
  <div id="footer">
     Copyright &copy; 2021 Paris Shopping <br>
